edf3ed4cf9c272f25f96698deb4803e538af5964
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-05 17:53:45 +0300

    chore: add map location

---
+++ data/map-location.ts
@@ -0,0 +1,12 @@
+export const MAP_CONFIG = {
+  name: "Sahwa Coffee",
+  address: "Bole Road, Addis Ababa, Ethiopia",
+  rating: 4.8,
+  reviewCount: "1,240",
+  embedUrl:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5!2d38.7578!3d9.032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8575e8e2decd%3A0x553ea40ff446dd6!2sBole%20Road%2C%20Addis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2set!4v1700000000000!5m2!1sen!2set",
+  directionsUrl:
+    "https://www.google.com/maps/dir/?api=1&destination=Sahwa+Coffee%2C+Bole+Road%2C+Addis+Ababa%2C+Ethiopia",
+  viewLargerUrl:
+    "https://www.google.com/maps/search/?api=1&query=Sahwa+Coffee%2C+Bole+Road%2C+Addis+Ababa%2C+Ethiopia",
+};
