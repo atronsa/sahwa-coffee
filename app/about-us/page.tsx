8f3aaba23662368f5f5e7f0a3492d4ce7b792eff
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-07 21:27:33 +0300

    feat: add about us page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/about/Hero";
+import WhoWeAre from "@/components/about/WhoWeAre";
+import MissionStatement from "@/components/about/MissionStatement";
+import OurBeginning from "@/components/about/OurBeginning";
+import ValuesGrid from "@/components/about/ValuesGrid";
+import CTA from "@/components/about/CTA";
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <WhoWeAre />
+      <MissionStatement />
+      <OurBeginning />
+      <ValuesGrid />
+      <CTA />
+    </>
+  );
+}
