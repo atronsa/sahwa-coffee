5abbae23813bbeb6a73fbeebc9d64610d5a78122
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-10 23:36:14 +0300

    chore: add nav links

---
+++ data/nav-links.ts
@@ -0,0 +1,11 @@
+export type Nav = {
+  label: string;
+  href: string;
+};
+
+export const NAV_LINKS: Nav[] = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "about-us" },
+  { label: "Product", href: "products" },
+  { label: "Contact", href: "contact" },
+];
