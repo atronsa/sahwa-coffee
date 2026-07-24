8d72dba7b872acab34c178d60f4c6944f0c38a74
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-24 20:43:17 +0300

    style: update padding x

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <>
       <Hero />
       <main className="relative bg-white overflow-hidden">
-        <div className="max-w-7xl mx-auto px-6 sm:px-8 md:px-10 py-8 sm:py-8 md:py-12 lg:py-16 grid grid-cols-1 md:grid-cols-2 gap-10 sm:gap-12 md:gap-16 lg:gap-20 items-start">
+        <div className="max-w-7xl mx-auto px-6 sm:px-10 md:px-12 lg:px-14 xl:px-20 py-12 sm:py-16 md:py-20 grid grid-cols-1 md:grid-cols-2 gap-10 sm:gap-12 md:gap-16 lg:gap-20 items-start">
           <Method />
           <div className="flex justify-center lg:justify-end w-full">
             <Form />
