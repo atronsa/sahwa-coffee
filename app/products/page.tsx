c7351c154565bde3c77fc366d9c13e536df7b7a5
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-24 20:42:01 +0300

    feat: add coffee grade component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,11 @@
 import Product from "@/components/product/Product";
+import CoffeeGrades from "@/components/product/CoffeeGrades";
 
 export default function Page() {
-  return <Product />;
+  return (
+    <>
+      <Product />
+      <CoffeeGrades />
+    </>
+  );
 }
