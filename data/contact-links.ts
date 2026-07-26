21127df2d4467354479c2ba31b712a4bfe0669f7
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-26 23:52:10 +0300

    chore: update phone no

--- data/contact-links.ts
+++ data/contact-links.ts
@@ -4,5 +4,5 @@ export type Contact = {
 };
 
 export const CONTACT_LINKS: Contact[] = [
-  { email: "hello@sahwacoffee.com", phone: "(111) 111-1111" },
+  { email: "hello@sahwacoffee.com", phone: "+251 940 404 422" },
 ];
