c8c522581f0bfad061a9c4746bdecb44e294cc0d
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-26 23:55:46 +0300

    feat: update the map location with address and coordinates

--- data/map-location.ts
+++ data/map-location.ts
@@ -1,12 +1,16 @@
 export const MAP_CONFIG = {
   name: "Sahwa Coffee",
-  address: "Bole Road, Addis Ababa, Ethiopia",
+  address: "Tirtira Building, Bole Atlas, Addis Ababa, Ethiopia",
   rating: 4.8,
   reviewCount: "1,240",
   embedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5!2d38.7578!3d9.032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8575e8e2decd%3A0x553ea40ff446dd6!2sBole%20Road%2C%20Addis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2set!4v1700000000000!5m2!1sen!2set",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.7206795426473!2d38.779571399999995!3d8.997829300000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85c0f32eb8e7%3A0x3f5074c3e1b18d10!2zVGlydGlyYSBCdWlsZGluZyB8IEJvbGUgQXRsYXMgfCDhibLhiK3hibLhiKsg4YiF4YqV4Yy7IHwg4Ymm4YiMIOGKoOGJteGIi-GItQ!5e0!3m2!1sen!2set!4v1785080813081!5m2!1sen!2set",
   directionsUrl:
-    "https://www.google.com/maps/dir/?api=1&destination=Sahwa+Coffee%2C+Bole+Road%2C+Addis+Ababa%2C+Ethiopia",
+    "https://www.google.com/maps/dir/?api=1&destination=Sahwa+Coffee%2C+Tirtira+Building%2C+Bole+Atlas%2C+Addis+Ababa%2C+Ethiopia",
   viewLargerUrl:
-    "https://www.google.com/maps/search/?api=1&query=Sahwa+Coffee%2C+Bole+Road%2C+Addis+Ababa%2C+Ethiopia",
+    "https://www.google.com/maps/search/?api=1&query=Sahwa+Coffee%2C+Tirtira+Building%2C+Bole+Atlas%2C+Addis+Ababa%2C+Ethiopia",
+  coordinates: {
+    lat: 8.9978293,
+    lng: 38.7795714,
+  },
 };
