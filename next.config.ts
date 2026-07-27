5a65f1cc055a74114f1259f0d0c431a99342c1e7
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-27 22:47:09 +0300

    feat: add standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compress: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react"],
+    memoryBasedWorkersCount: true,
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
